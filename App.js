bc58cb918df70d21b18494a4050df5f1032e3977
Pedro Collin <67438145+PedroCollin@users.noreply.github.com>
2021-08-12 10:31:53 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  ImageBackground,
+} from 'react-native';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={{
-        backgroundColor: 'white',
-        width: '50%',
-        height:300,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-        <Text>SENAI</Text>
-      </View>
-      <View>
-        <Text>Turma da BOSCH!</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={{flex: 1}}
+        source={{
+          uri:
+            'https://saopaulofc.com.br/wp-content/uploads/2020/11/luciano-1.jpg',
+        }}>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+             THE BOYS OF BOSCH
+          </Text>
+          <View style={styles.contentCenter}>
+            <Image
+              source={{
+                uri:
+                  'https://media4.giphy.com/media/9oHZQ2gEez8ti/giphy.gif',
+              }}
+              style={{
+                width: 60,
+                height: 60,
+                marginTop: 100
+              }}
+            />
+            <Text style={styles.textStyle}>
+              REACT É O BRAIA
+            </Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#66ccff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+    padding: 15,
+    color: 'white',
+    fontWeight: 'bold',    
+    textAlign: 'center',    
+  },
+  contentCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    padding: 10,
+  }
 });
