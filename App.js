fe0b96ed168ddd79b9c6e55cfc6f8d7cd1cb8184
Pedro Collin <67438145+PedroCollin@users.noreply.github.com>
2021-08-19 07:57:19 -0300

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StatusBar, Text, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 
 export default class App extends Component {
 
