69ed69347b557235cd0ffd40f4d32de5c1b37bb2
Pedro Collin <67438145+PedroCollin@users.noreply.github.com>
2021-08-18 21:18:17 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,79 +1,42 @@
-import React from 'react';
-
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  SafeAreaView,
-  Button,
-  Alert,
- // ImageBackground,
-} from 'react-native';
-
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        style={{flex: 1}}
-        source={{
-          uri:
-            'https://saopaulofc.com.br/wp-content/uploads/2020/11/luciano-1.jpg',
-        }}>
-        <View style={styles.container}>
-          <Text style={styles.title}>
-             THE BOYS OF BOSCH
-          </Text>
-          <View style={styles.contentCenter}>
-            <Image
-              source={{
-                uri:
-                  'https://media4.giphy.com/media/9oHZQ2gEez8ti/giphy.gif',
-              }}
-              
-              style={{
-                width: 60,
-                height: 60,
-                marginTop: 100,
-              }}
-            />
-            
-            <Text style={styles.textStyle}>
-              REACT É O BRAIA
-            </Text>
-          </View>
-        </View>
-       </ImageBackground> 
-      <Button
-        title="Alterar Cor"
-        onPress={() => Alert.alert('Cor alterada')}
-        onClick={(event) => onClick(id, acao, color)}      
-          />
-    </SafeAreaView>
+import React, { useState } from "react";
+const colors = ["red", "green", "blue", "yellow"];
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("");
+return (
+    <div
+      className="App"
+      style={{
+        backgroundColor
+      }}
+    >
+      <style>
+        {`
+        .circle {
+          border-radius: 50%;
+          width: 50px;
+          height: 50px;
+          border: 1px solid black;
+          display: inline-block;
+          cursor: pointer;
+        }
+#screen {
+          width: 100vw;
+          height: 100vh;
+        }
+      `}
+      </style>
+      {colors.map((c) => {
+        return (
+          <div
+            key={c}
+            style={{
+              backgroundColor: c
+            }}
+            class="circle"
+            onClick={() => setBackgroundColor(c)}
+          ></div>
+        );
+      })}
+    </div>
   );
-};
-export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 25,
-    padding: 15,
-    color: 'white',
-    fontWeight: 'bold',    
-    textAlign: 'center',    
-  },
-  contentCenter: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textStyle: {
-    color: 'white',
-    padding: 10,
-  }
-});
-
-//function Click(id: string)
-
-
+}
